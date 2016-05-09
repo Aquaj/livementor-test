2009a62756c1e17fae29f4d42445a7ac540e8c91
Jeremie <jeremie.bonal@gmail.com>
2016-05-09 09:56:57 +0200

    Basic argument input.

--- test1-rubyjson/json2csv.rb
+++ test1-rubyjson/json2csv.rb
@@ -4,6 +4,10 @@ require 'csv'
 
 def json2csv(file_path)
   json = JSON.parse(open(file_path).read())
+
 end
 
-json2csv("https://gist.githubusercontent.com/gregclermont/ca9e8abdff5dee9ba9db/raw/7b2318efcf8a7048f720bcaff2031d5467a4a2c8/users.json")
+argument = ARGV.length > 0 ? ARGV[0] : "https://gist.githubusercontent.com/gregclermont/ca9e8abdff5dee9ba9db/raw/7b2318efcf8a7048f720bcaff2031d5467a4a2c8/users.json"
+
+json2csv(argument)
+p argument
